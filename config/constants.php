f01d90b66204a3c6f71db608ce10c7826c231770
MonsteDev <95182974+monstedev@users.noreply.github.com>
2024-08-22 11:01:00 +0200

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -4,5 +4,5 @@ define('ROOT__URL', ''); // inserire l'url di index.php (es. localhost/MyGym/ )
 define('DB__HOST', 'localhost'); // cambiare a seconda del proprio host
 define('DB_USER', ''); // inserire il nome utente del proprio account di phpmyadmin
 define('DB_PASS', ''); // inserire la password
-define('DB_NAME', 'mygym'); // nome del database a vostra discrezione
+define('DB_NAME', 'mygym'); // nome del database a vostra scelta ( deve essere lo stesso di database.sql )
 ?>
