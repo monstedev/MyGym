06340e9db963f9a1000bd5933fa18bbd643c5c6b
MonsteDev <95182974+monstedev@users.noreply.github.com>
2024-08-22 11:00:17 +0200

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -9,7 +9,7 @@ require 'config/database.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyGym - Riccardo</title>
+    <title>MyGym</title>
     <link rel="shortcut icon" href="img/m-logo-zip-file/svg/logo-no-background.svg">
     <link rel="stylesheet" href="style.css">
 
